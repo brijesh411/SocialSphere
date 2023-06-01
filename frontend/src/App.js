0f20357e1389cb92d3fd0fc7779eca4c00dda43d
Brijesh Rajput <rbk4112001@gmail.com>
2023-06-01 09:23:34 +0530

    first over

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import PageRender from "./PageRender";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import Home from "./components/Home";
 
 const App = () => {
   return(
-    <div>
-      Home
-    </div>
+    <Router>
+      <input type="checkbox" id="theme" />
+      <div className='App'>
+        <div className="main">
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
+        </div>   
+      </div>
+    </Router>
   )
 }
 
